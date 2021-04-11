9a6113024421f0d8d9aaf3b25ae7a665b51af1b5
Sergio-DC <sgdc377@gmail.com>
2021-04-11 12:32:23 -0500

    Sample input data for api added
    This is intended to let know the devs how to use the api

---
+++ src/routes/petRoutes.ts
@@ -0,0 +1,16 @@
+import { Router, Request, Response } from 'express';
+import Pet from "../models/petModel";
+const router = Router();
+
+router.post('/pet', (req: Request, res: Response) => {
+    let body = req.body;     
+    const pet = new Pet(body); 
+    pet.save((err, petDoc) => {
+        if(err) 
+            return res.status(500).json(err);
+        res.status(200)
+            .json({
+                message: 'Pet registered'
+            })
+    })
+})
