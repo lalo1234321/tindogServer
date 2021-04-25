17adbc34518247098fce78294da159a865743dba
lalo1234321 <edominguezc@toluca.tecnm.mx>
2021-04-25 17:18:41 -0500

    extracting the logic to the middleware folder

---
+++ src/controllers/auth.ts
@@ -0,0 +1,43 @@
+import { Request, Response } from 'express';
+import User from '../models/userModel'; 
+import * as bcrypt from 'bcrypt';
+import { IUser } from "../interfaces/IUser";
+const { generarJWT } = require('../utils/jwt');
+
+const login = async(req: Request, res: Response) => {
+    let passwordBody = req.body.password;
+    
+    // para obtener el valor de un query si es que no implementamos una interfaz en nuestros modelos
+    //  de mongodb
+    // let password2 = query.get('password');
+    
+    try {
+        const user:IUser = await User.findOne({email: req.body.email});
+        if( !user ) {
+            return res.status(400).json({
+                msg:"Usuario no encontrado"
+            });
+        }
+        const validPassword = await bcrypt.compare(passwordBody, user.password);
+        if( !validPassword ) {
+            return res.status(400).json({
+                msg:'Las credenciales no coinciden'
+            });
+        }
+        const token = await generarJWT( user._id );
+        res.status(200).json({
+            user,
+            token
+        });
+
+    } catch( err ) {
+        return res.status(500).json({
+            msg:'Ocurrió un error, hablé con el administrador!'
+        });
+    }
+
+}
+
+export {
+    login
+}
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,54 +1,9 @@
-import { Router, Request, Response } from 'express';
-import User from '../models/userModel'; 
-import * as bcrypt from 'bcrypt';
-import { IUser } from "../interfaces/IUser";
-// import { generarJWT } from '../utils/jwt';
-const { generarJWT } = require('../utils/jwt');
+import { Router } from 'express';
+import { login } from '../controllers/auth';
 
 const router = Router();
 
-
-router.post('/login', async(req, res) => {
-    let passwordBody = req.body.password;
-    
-    
-    // let password2 = query.get('password');
-    
-    try {
-        const user:IUser = await User.findOne({email: req.body.email});
-        if( !user ) {
-            return res.status(400).json({
-                msg:"Usuario no encontrado"
-            });
-        }
-        const validPassword = await bcrypt.compare(passwordBody, user.password);
-        if( !validPassword ) {
-            return res.status(400).json({
-                msg:'Las credenciales no coinciden'
-            });
-        }
-        const token = await generarJWT( user._id );
-        res.status(200).json({
-            user,
-            token
-        });
-
-    } catch( err ) {
-        return res.status(500).json({
-            msg:'Ocurrió un error, hablé con el administrador!'
-        });
-    }
-
-
-    //bcrypt.compare(req.body.password, user.password);
-    //bcrypt.compare();
-    // password = await bcrypt.compare(password, user.password);
-    // if(!password) {
-    //     return res.json({
-    //         msg: "algo salio mal"
-    //     });
-    // }
-});
+router.post('/login', login);
 
 
 
