fc04352683cebe0aa74824724e855b8cec212b86
cabra5262@outlook.com <cabra5262@outlook.com>
2021-06-28 21:40:11 -0500

    little modifications

--- src/routes/salesRoutes.ts
+++ src/routes/salesRoutes.ts
@@ -1,20 +1,9 @@
-import { Router, Request, Response } from 'express';
+import { Router } from 'express';
 import { registerSales } from '../controllers/registerSales';
 
 const router = Router();
 
-//explorar las ventas
-// router.get('/sales', (req: Request, res: Response) => {
-//     Sales.find({status: "Disponible"},(err, salesDoc) => {
-//         if (err) {
-//             console.error(err);
-//         }
-//         return res.status(200).json({
-//             sales: salesDoc
-//         });
-//     });
-// });
-
 router.post('/sales/register', registerSales);
+router.get('/sales/confirmation');
 
 export default router;
