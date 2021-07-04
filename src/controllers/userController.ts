33f3c51cdbe2a0aba9d0996fc4689f7cb3a62204
Jaki657 <postJK128@gmail.com>
2021-07-04 17:06:27 -0500

    ...

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -64,8 +64,8 @@ export const registerUser = async (req: Request, res: Response) => {
     }
 }
 
-export const getAllPetsOwnedByUser = (req: Request, res: Response) => {
-    let query = User.find({ _id: req.userId }).populate('ownedPets')
+export const getAllPetsOwnedByUser = async (req: Request, res: Response) => {
+    /*let query = User.find({ _id: req.userId }).populate('ownedPets')
     query.select('ownedPets').exec((err, userDoc) => {
         if (err)
             return res.status(404).json({
@@ -74,19 +74,19 @@ export const getAllPetsOwnedByUser = (req: Request, res: Response) => {
         res.json(
             userDoc
         )
-    })
+    })*/
 
-    /*Pet.find({ owner: req.userId, isDeleted: false }, (err, petDoc) => {
+    await Pet.find({ owner: req.userId, isDeleted: false }, (err, petDoc) => {
         if (err) {
             return res.status(404).json({
                 message: err
             });
         } else {
-            return res.status(404).json([{
+            return res.status(200).json([{
                 ownedPets: petDoc
             }]);
         }
-    });*/
+    });
 }
 
 export const updatePassword = async (req: Request, res: Response) => {
