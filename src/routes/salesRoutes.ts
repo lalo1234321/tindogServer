effaf527b5cf8985a7225bf5647e761286e75fef
cabra5262@outlook.com <cabra5262@outlook.com>
2021-07-07 13:42:05 -0500

    add numeric validator

--- src/routes/salesRoutes.ts
+++ src/routes/salesRoutes.ts
@@ -1,10 +1,11 @@
 import { Router, Request, Response } from 'express';
 import { validateJWT } from '../middleware/validateJWT';
 import { registerSales, getAllSales, deleteSales, makePurchase} from '../controllers/registerSales';
+import {validatePrice} from '../middleware/validateBodyFields';
 
 const router = Router();
 
-router.post('/sales/register', registerSales);
+router.post('/sales/register', validatePrice, registerSales);
 router.get('/sales/confirmation');
 router.get('/getAllSales', [validateJWT], getAllSales);
 router.put('/deleteSales/:saleId', [validateJWT], deleteSales);
