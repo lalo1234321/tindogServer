c2a0c84de2c80b25ed229ed8d6a2c1a0d4b5f50e
Jaki657 <postJK128@gmail.com>
2021-07-08 01:41:17 -0500

    datos sesion

--- src/controllers/socket.ts
+++ src/controllers/socket.ts
@@ -3,9 +3,9 @@ import { IUser } from "../interfaces/IUser";
 import Notification from '../mongoose-models/notificationModel';
 import Message from '../mongoose-models/messageModel';
 
-const userOnline = async( uid = '' ) => {
+const userOnline = async (uid = '') => {
     //console.log(uid);
-    const user:IUser = await User.findById(uid);
+    const user: IUser = await User.findById(uid);
     user.isOnline = true;
     await user.save();
     return user;
@@ -13,22 +13,23 @@ const userOnline = async( uid = '' ) => {
 
 
 
-const userOffline = async( uid = '' ) => {
-    const user:IUser = await User.findById(uid);
+const userOffline = async (uid = '') => {
+    const user: IUser = await User.findById(uid);
     user.isOnline = false;
+    user.auxLastConnection = new Date();
     await user.save();
     return user;
 }
 
-const registerNotification = async( petFrom = '', petTo = '' ) => {
-    const notification = new  Notification({
+const registerNotification = async (petFrom = '', petTo = '') => {
+    const notification = new Notification({
         to: petTo,
         from: petFrom
     });
     await notification.save();
-}   
+}
 
-const registerMessage = async(petFrom = '', petTo = '', msg = '') => {
+const registerMessage = async (petFrom = '', petTo = '', msg = '') => {
     const message = new Message({
         from: petFrom,
         to: petTo,
